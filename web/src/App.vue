2adbb76486e22039dfee6a0f74e004d790bd41fc
Xavi Lee <awxiaoxian2020@163.com>
2022-07-12 22:00:21 +0800

    init for end seperation

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,29 @@
 <template>
-  <router-view/>
+  <div>
+    <div>Bot昵称 :{{ bot_name }}</div>
+    <div>Bot战力 :{{ bot_rating }}</div>
+  </div>
 </template>
+<script>
+import { getCurrentInstance } from "vue";
+import { ref } from "vue";
 
-<style>
-
-</style>
+export default {
+  name: "App",
+  setup: () => {
+    const instance = getCurrentInstance();
+    const that = instance.appContext.config.globalProperties;
+    let bot_name = ref("");
+    let bot_rating = ref("");
+    that.$axios.get("http://127.0.0.1:3000/pk/getBotInfo").then(
+      (response) => {
+      bot_name.value = response.data.name;
+      bot_rating.value = response.data.rating;
+    });
+    return {
+      bot_name,
+      bot_rating,
+    };
+  },
+};
+</script>
