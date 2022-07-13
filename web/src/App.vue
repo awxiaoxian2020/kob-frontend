a6ea08cbc29607305e67ce245e788f0b54c33efd
Xavi Lee <awxiaoxian2020@163.com>
2022-07-13 11:51:40 +0800

    add NavBar

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <div>
-    <div>Bot昵称 :{{ bot_name }}</div>
-    <div>Bot战力 :{{ bot_rating }}</div>
-  </div>
+  <NavBar/>
 </template>
 <script>
-import { getCurrentInstance } from "vue";
-import { ref } from "vue";
+import NavBar from './components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+
+
 
 export default {
-  name: "App",
-  setup: () => {
-    const instance = getCurrentInstance();
-    const that = instance.appContext.config.globalProperties;
-    let bot_name = ref("");
-    let bot_rating = ref("");
-    that.$axios.get("http://127.0.0.1:3000/pk/getBotInfo").then(
-      (response) => {
-      bot_name.value = response.data.name;
-      bot_rating.value = response.data.rating;
-    });
-    return {
-      bot_name,
-      bot_rating,
-    };
-  },
+  name: 'App',
+  components:{
+    NavBar,
+  }
 };
 </script>
 <style>
---
+++ web/src/components/NavBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">King of Bots</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">对战</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">对局列表</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">排行榜</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              chuercs
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">我的 Bot</a></li>
+              <li><a class="dropdown-item" href="#">退出</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped></style>
